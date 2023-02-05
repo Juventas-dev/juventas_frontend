97176288e20bcf0dc8345052f9c0957e2ce8b28c
kim2ju <bamtori05@naver.com>
2023-02-05 12:05:05 +0900

    네이버 로그인 구현 완료

--- src/pages/Mypage.tsx
+++ src/pages/Mypage.tsx
@@ -1,14 +1,24 @@
 import React from 'react';
 import {View, Pressable, Text} from 'react-native';
 import {logout, unlink} from '@react-native-seoul/kakao-login';
+import NaverLogin from '@react-native-seoul/naver-login';
 import {useAppDispatch} from '../store';
 import userSlice from '../slices/user';
+import {RootState} from '../store/reducer';
+import {useSelector} from 'react-redux';
 
 function Mypage() {
   const dispatch = useAppDispatch();
-  const signOutWithKakao = async (): Promise<void> => {
-    const message = await logout();
+  const loginType = useSelector((state: RootState) => state.user.loginType);
+  const signOutUser = async (): Promise<void> => {
+    if (loginType === 'kakao') {
+      const message = await logout();
 
+      console.log(message);
+    } else if (loginType === 'naver') {
+      await NaverLogin.logout();
+      console.log('네이버 로그아웃 완료');
+    }
     dispatch(
       userSlice.actions.setUser({
         name: '',
@@ -16,13 +26,16 @@ function Mypage() {
         loginType: '',
       }),
     );
-
-    console.log(message);
   };
 
-  const unlinkKakao = async (): Promise<void> => {
-    const message = await unlink();
-
+  const unlinkUser = async (): Promise<void> => {
+    if (loginType === 'kakao') {
+      const message = await unlink();
+      console.log(message);
+    } else if (loginType === 'naver') {
+      await NaverLogin.deleteToken();
+      console.log('네이버 회원탈퇴 완료');
+    }
     dispatch(
       userSlice.actions.setUser({
         name: '',
@@ -30,16 +43,14 @@ function Mypage() {
         loginType: '',
       }),
     );
-
-    console.log(message);
   };
 
   return (
     <View>
-      <Pressable onPress={signOutWithKakao}>
+      <Pressable onPress={signOutUser}>
         <Text>로그아웃</Text>
       </Pressable>
-      <Pressable onPress={unlinkKakao}>
+      <Pressable onPress={unlinkUser}>
         <Text>회원 탈퇴</Text>
       </Pressable>
     </View>
