5c05669f2b5c7f8700cb5ae23fbc01016ea35684
kim2ju <bamtori05@naver.com>
2023-05-19 12:46:05 +0900

    푸시알림, 오류 수정

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,4 +1,4 @@
-import React, {useCallback, useRef, useState} from 'react';
+import React, {useCallback, useRef, useState, useEffect} from 'react';
 import {
   Text,
   TextInput,
@@ -23,9 +23,11 @@ import {useAppDispatch} from '../store';
 import userSlice from '../slices/user';
 import Config from 'react-native-config';
 import {KeyboardAwareScrollView} from 'react-native-keyboard-aware-scroll-view';
+import DeviceInfo from 'react-native-device-info';
 const IconBasic = require('../../assets/image/fund.png');
 const kakao = require('../../assets/image/KakaoTalk.png');
 const naver = require('../../assets/image/Naver.png');
+import PushNotification from 'react-native-push-notification';
 
 type SignInScreenProps = NativeStackScreenProps<RootStackParamList, 'SignIn'>;
 
@@ -38,6 +40,17 @@ function SignIn({navigation}: SignInScreenProps) {
   const PassRef = useRef<TextInput | null>(null);
   const [alertPhoneNum, setAlertPhoneNum] = useState(false);
   const [alertPass, setAlertPass] = useState(false);
+  const [deviceToken, setDeviceToken] = useState('');
+
+  useEffect(() => {
+    PushNotification.configure({
+      // (optional) 토큰이 생성될 때 실행됨(토큰을 서버에 등록할 때 쓸 수 있음)
+      onRegister: function (token: any) {
+        setDeviceToken(token.token);
+        console.log('TOKEN:', token.token);
+      },
+    });
+  }, []);
 
   const onChangePhone = useCallback((text: string) => {
     setPhone(text.trim());
@@ -77,6 +90,11 @@ function SignIn({navigation}: SignInScreenProps) {
         'refreshToken',
         response.data.refreshToken,
       );
+
+      await axios.post(`${Config.API_URL}/push/register`, {
+        userId: response.data.id,
+        deviceToken: deviceToken,
+      });
     } catch (error) {
       const errorResponse = (
         error as AxiosError<{error: string; message: string}>
